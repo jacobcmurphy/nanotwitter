8e64a57f919c36de5eaae0833955a7f6cc26fa47
Ryan Marcus <ryan@rmarcus.info>
2015-03-15 15:02:17 -0400

    added CORS

--- tweet_service/app.rb
+++ tweet_service/app.rb
@@ -1,34 +1,49 @@
 require 'sinatra'
 require 'sequel'
 require 'json'
+require 'sinatra/cross_origin'
 
 dbloc = ENV['database'] || 'sqlite://dev.db'
 
 db = Sequel.connect(dbloc)
 tweets = db.from(:tweets)
 
+configure do
+  enable :cross_origin
+end
+
+options "*" do
+  response.headers["Allow"] = "HEAD,GET,PUT,DELETE,OPTIONS"
+  
+  # Needed for AngularJS
+  response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+  
+  200
+end
+
 post '/tweet' do					#if post at /tweet
-	#puts DB.from(:tweets).all()
-	tweet = JSON.parse request.body.read		#receives tweet
-	tweet['created_at'] = Time.now			#adds timestamp
-	begin
-		tweets.insert(tweet)			#inserts tweet
-		200					#good response
-	rescue
-		400					#malformed
-	end
+  #puts DB.from(:tweets).all()
+  tweet = JSON.parse request.body.read		#receives tweet
+  tweet['created_at'] = Time.now			#adds timestamp
+  begin
+    tweets.insert(tweet)			#inserts tweet
+    200					#good response
+  rescue
+    400					#malformed
+  end
 end
 
 get '/recent' do
   puts "doing fetch"
   begin
-    db.fetch("SELECT * FROM tweets ORDER BY created_at LIMIT 20").all()
+    db.fetch("SELECT * FROM tweets ORDER BY created_at LIMIT 20").all().to_json
+
   rescue
     "{}"
   end
 end
 
 get '/tweet' do						#allows to search by user
-	user = params[:user]
-	"#{tweets.where(:user => user).all()}"
+  user = params[:user]
+  "#{tweets.where(:user => user).all()}"
 end
