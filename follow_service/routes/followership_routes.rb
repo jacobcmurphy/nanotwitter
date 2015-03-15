307edef8257ba3d2f56a0e620de08a673da90fb7
Ryan Marcus <ryan@rmarcus.info>
2015-03-15 16:27:01 -0400

    Fixed a bug with parameters, I think?

--- follow_service/routes/followership_routes.rb
+++ follow_service/routes/followership_routes.rb
@@ -1,23 +1,44 @@
 require 'sinatra/base'
 require 'sinatra/activerecord'
+require 'sinatra/cross_origin'
 require_relative "../models/followership"
 
 class FollowershipRoutes < Sinatra::Base
-	register Sinatra::ActiveRecordExtension
+  register Sinatra::ActiveRecordExtension
+  register Sinatra::CrossOrigin
 
-	# get all followers of user with user_id
-	get '/:user_id' do
-		results = {user_id: params[:user_id]}
-		results[:followees] = Followership.where(user_id: params[:user_id]).select("followee_id").map{|followership| followership.followee_id}
-		status 200
-		results.to_json
-	end
+  configure do
+    enable :cross_origin
+  end
 
-	# insert a new tag
-	put '/:user_id' do
-		followership = Followership.create(user_id: params[:user_id], followee_id: params[:followee_id]) if params[:followee_id]
-		status 201
-		followership.to_json
-	end
-			
+  options "*" do
+    response.headers["Allow"] = "HEAD,GET,PUT,DELETE,OPTIONS"
+    response.headers["Access-Control-Allow-Methods"] = "HEAD,GET,PUT,DELETE,OPTIONS"
+    
+    # Needed for AngularJS
+    response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+    
+    200
+  end
+  
+  # get all followers of user with user_id
+  get '/:user_id' do
+    cross_origin
+    results = {user_id: params[:user_id]}
+    results[:followees] = Followership.where(user_id: params[:user_id]).select("followee_id").map{|followership| followership.followee_id}
+    status 200
+    results.to_json
+  end
+
+  # insert a new tag
+  put '/:user_id' do
+    cross_origin
+    user_id_val = params[:user_id]
+    followee_id_val = JSON.parse(request.body.read)["followee_id"]
+
+    followership = Followership.create(user_id: user_id_val, followee_id: followee_id_val)
+    status 201
+    followership.to_json
+  end
+  
 end
