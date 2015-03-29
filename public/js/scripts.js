e160f8feeb633f62a29cf7d60cce61732f633f4b
Eden Zik <edenzik@gmail.com>
2015-03-28 20:00:13 -0400

    improved UI, added get script

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,12 +1,25 @@
 
 $(document).ready(function(){/* off-canvas sidebar toggle */
 
-$('[data-toggle=offcanvas]').click(function() {
-  	$(this).toggleClass('visible-xs text-center');
-    $(this).find('i').toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
-    $('.row-offcanvas').toggleClass('active');
-    $('#lg-menu').toggleClass('hidden-xs').toggleClass('visible-xs');
-    $('#xs-menu').toggleClass('visible-xs').toggleClass('hidden-xs');
-    $('#btnShow').toggle();
+	$('[data-toggle=offcanvas]').click(function() {
+		$(this).toggleClass('visible-xs text-center');
+		$(this).find('i').toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
+		$('.row-offcanvas').toggleClass('active');
+		$('#lg-menu').toggleClass('hidden-xs').toggleClass('visible-xs');
+		$('#xs-menu').toggleClass('visible-xs').toggleClass('hidden-xs');
+		$('#btnShow').toggle();
+	});
 });
+
+$(function () {
+	$('.btnChangeXML').click(function () {
+		hello = jQuery.get('/', function(data) {
+			alert(data);
+			//process text file line by line
+			$('#div').html(data.replace('n','<br />'));
+			}); 
+		$('#txtDisplay').val(hello);
+	});
+
 });
+
