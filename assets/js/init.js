a7ec022e9d40be32e408598a1c4ef059b9bca2a7
Eden Zik <edenzik@gmail.com>
2015-04-30 19:37:33 -0400

    bug

--- assets/js/init.js
+++ assets/js/init.js
@@ -17,14 +17,6 @@ var cachedFollowing = null;
 if (id!=null){
 	//alert("cookie workinggg");
 	$('#signIn').hide();
-	$('#signIn').fadeOut(300, function(){
-		alert("cmore work");
-
-		$('#postTweet').fadeIn(300);
-		loadFollowers();
-		loadFollowing();
-		$('#signOut').fadeIn(300);
-	});
 }
 
 
