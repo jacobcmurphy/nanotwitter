84a0dc05cd1494e2c5373063ce7b031638deb30d
Eden Zik <edenzik@gmail.com>
2015-04-30 17:13:49 -0400

    added static assets

---
+++ assets/js/init.js
@@ -0,0 +1,431 @@
+$( document ).ready(function() {
+	$('#postTweet').hide();
+	$('#register').hide();
+	$('#signOut').hide();
+	$('#signIn').show();
+});
+
+
+
+var id = readCookie("id");
+var email = readCookie("email");
+var password = readCookie("password");
+
+var cachedFollowing = null;
+
+
+if (id!=null){
+	$('#signIn').fadeOut(300, function(){
+		$('#postTweet').fadeIn(300);
+		loadFollowers();
+		loadFollowing();
+		$('#signOut').fadeIn(300);
+	});
+}
+
+
+function createCookie(name, value, days) {
+	var expires;
+
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		expires = "; expires=" + date.toGMTString();
+	} else {
+		expires = "";
+	}
+	document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = encodeURIComponent(name) + "=";
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) === 0) return decodeURIComponent(c.substring(nameEQ.length, c.length));
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name, "", -1);
+}
+
+String.prototype.format = function() {
+	var formatted = this;
+	for (var i = 0; i < arguments.length; i++) {
+		var regexp = new RegExp('\\{'+i+'\\}', 'gi');
+		formatted = formatted.replace(regexp, arguments[i]);
+	}
+	return formatted;
+};
+
+(function($) {
+	$.fn.clickToggle = function(func1, func2) {
+		var funcs = [func1, func2];
+		this.data('toggleclicked', 0);
+		this.click(function() {
+			var data = $(this).data();
+			var tc = data.toggleclicked;
+			$.proxy(funcs[tc], this)();
+			data.toggleclicked = (tc + 1) % 2;
+		});
+		return this;
+	};
+}(jQuery));
+
+jQuery.each( [ "put", "delete" ], function( i, method ) {
+	jQuery[ method ] = function( url, data, callback, type ) {
+		if ( jQuery.isFunction( data ) ) {
+			type = type || callback;
+			callback = data;
+			data = undefined;
+		}
+
+		return jQuery.ajax({
+			url: url,
+		       type: method,
+		       dataType: type,
+		       data: data,
+		       success: callback
+		});
+	};
+});
+
+
+function toUser(eleID, name,count){
+	return '<li><a class="widget-list-link" id={0}dialog><img src="http://edenzik.github.io/nanotwitter/public/img/icon.png"><h7 id="{1}dialog">{1}</h7><span>{2} followers</a></span></span></li>'.format(eleID, name,count);
+};
+
+function toTweet(name,text,date){
+	return '<section class="notif notif-notice"><a><h6 class="{0}tweet notif-title">{0}</h6></a><p>{1}<div class="tweet_date">{2}</div></section>'.format(name,text,date);
+};
+
+function warningBox(warning){
+	return '<div id="warning" class="alert alert-warning"><a href="#" class="close" data-dismiss="alert">&times;</a>{0}</div>'.format(warning);
+}
+
+function toUserBox(eleID, name, content){
+	if (content==""){
+		content = "<h1>No tweets to show</h1>";
+	}
+	return '<div id="{0}" class="modal fade"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button><h4 class="modal-title">{1}\'s Tweets</h4><input id="{0}follow" type="follow" class="follow-button" value="Follow"></div>{2}</div>'.format(eleID, name, content);
+}
+
+
+
+$('#registerConfirmButton').click(function() {
+	$('#warning').remove();
+	var isValidPass = validPassword($('#regPass1').val(),$('#regPass2').val());
+	if (!isValidPass){
+		$(warningBox("Bad Password")).appendTo('#register');
+		$('#register').effect("shake");
+		return;
+	}
+	email = $('#regEmail').val();
+	password = $('#regPass1').val();
+	$.post( "/account/register", {
+		username:$('#username').val(),
+		email:email,
+		password:password
+	},
+	function( data ) {
+		data = JSON.parse(data);
+		if (data.status == "USER CREATED") {
+			$('#register').fadeOut(300, function(){
+				id = data.id;
+				$('#postTweet').fadeIn(300);
+				loadFollowers();
+				loadFollowing();
+				$('#email').val("");
+				$('#password').val("");
+				$('#signOut').fadeIn(300);
+				createCookie("email",email,1);
+				createCookie("password",password,1);
+				createCookie("id",id,1);
+			});
+			return;
+		}
+		if (data.status == "USER EXISTS"){
+			$(warningBox("USER EXISTS")).appendTo('#register');
+		} else {
+			$(warningBox("Something went wrong...")).appendTo('#register');
+		}
+		$('#register').effect("shake");
+		return;
+	});
+
+});
+
+
+
+$('#signInButton').click(function() {
+	$('#warning').remove();
+	email = $('#email').val();
+	password = $('#password').val();
+	if (email=="" || password==""){
+		$('#signIn').effect("shake");
+		return;
+	}
+	$.post( "/account/login", {
+		email:email,
+		password:password
+	},
+	function( data ) {
+		data = JSON.parse(data);
+		if (data.status == "OK") {
+			$('#signIn').fadeOut(300, function(){
+				id = data.id;
+				$('#postTweet').fadeIn(300);
+				loadFollowers();
+				loadFollowing();
+				$('#email').val("");
+				$('#password').val("");
+				$('#signOut').fadeIn(300);
+				createCookie("email",email,1);
+				createCookie("password",password,1);
+				createCookie("id",id,1);
+				loadTweetsOfFollowers();
+			});
+			return;
+		};
+		$('#signIn').effect("shake");
+		email = null;
+		password = null;
+		if (data.status == "BAD PASSWORD"){			
+			$(warningBox("Bad Password")).appendTo('#signIn');
+			return;
+		}
+		if (data.status == "NOT FOUND") {
+			$(warningBox("User does not exist")).appendTo('#signIn');
+			return;
+		}
+	});
+});
+
+
+
+$('#postTweet').tweetbox({
+	limit:140, 
+});
+
+
+
+function validPassword(pass1, pass2){
+	if (pass1.length < 8) return false;
+	return pass1==pass2;
+}
+
+$('#signOutButton').click(function() {
+	id = null;
+	email = null;
+	password = null;
+	$('#signOut').fadeOut(100);
+	$('#postTweet').fadeOut(100, function() {
+		
+			$('#signIn').fadeIn(100, function(){
+				$('#followers').empty();
+				$('#following').empty();
+				eraseCookie("id");
+				eraseCookie("email");
+				eraseCookie("password");
+				loadTweets();
+
+			});
+	});
+	$(".follow-button").hide();
+
+});
+
+
+
+$('#registerButton').click(function() {
+	$('#signIn').fadeOut(300, function(){
+		$('#register').fadeIn(300);
+	});
+
+});
+
+$('#tweet-box-button').click(function(){
+	var tweet = $('#editor').html();
+	$.post( "/api/v1/tweet", {
+		text: tweet,
+		email: email,
+		password: password
+	});
+	loadTweets();
+	$(this).fadeOut(500, function() {
+		$('#editor').html("");
+	});
+	$(this).fadeIn(1000)
+});
+
+loadTweets();
+
+
+function loadTweets(){
+	$.get("/api/v1/tweet", function(data){
+		populateTweets(JSON.parse(data));
+	});
+}
+
+function loadTweetsOfFollowers(){
+	$.get("/api/v1/tweet/from/"+id, function(data){
+		populateTweets(JSON.parse(data));
+	});
+}
+
+loadAll();
+
+function loadAll(){
+	$.get("api/v1/user/", function(data){
+		populateAll(JSON.parse(data));
+	});
+}
+
+function loadFollowers(){
+	$('#followers').empty();
+	$.get("api/v1/follow/to/"+id, function(data){
+		populateFollowers(JSON.parse(data));
+	});
+}
+
+function loadFollowing(){
+	$('#following').empty();
+	cachedFollowing = [];
+	$.get("api/v1/follow/from/"+id, function(data){
+		var data = JSON.parse(data);
+		data.map(function(user){
+			cachedFollowing.push(user.followee_id);	
+		});
+		populateFollowing(data);
+	});
+}
+
+function loadTweetsOfUser(user_id, username, follower){
+	$('#'+user_id+'dialog').remove();
+	var tweetsOfUser = "";
+	$.get("api/v1/tweet/"+user_id, function(data){
+		var tweets = JSON.parse(data);
+		for (tweet in tweets){
+			tweetsOfUser += toTweet(tweets[tweet].username,tweets[tweet].text,tweets[tweet].created);
+		}
+
+		$(document.body).append(toUserBox(user_id+'dialog', username, tweetsOfUser));
+		$("#" + user_id + 'dialog').modal('show');
+		if (id==null){
+			$('#' + user_id + 'dialogfollow').hide();
+		}
+		else {
+			$('#' + user_id + 'dialogfollow').show();
+		}
+	var to_follow = function(){
+		$.post("/api/v1/follow/to/" + user_id, {id:id, email:email,password:password});
+		loadFollowing();
+		loadFollowers();
+		$('#' + user_id + 'dialogfollow').val("Unfollow");
+	};
+	var to_unfollow = function(){
+		$.delete("/api/v1/follow/to/" + user_id, {id:id, email:email,password:password});
+		loadFollowing();
+		loadFollowers();
+		$('#' + user_id + 'dialogfollow').val("Follow");
+	}
+	if ($.inArray(user_id, cachedFollowing) == 0){
+		$('#' + user_id + 'dialogfollow').val("Unfollow");
+		$('#' + user_id + 'dialogfollow').clickToggle(to_unfollow,to_follow);
+	}
+	else {
+		$('#' + user_id + 'dialogfollow').val("Follow");
+		$('#' + user_id + 'dialogfollow').clickToggle(to_follow,to_unfollow);
+	}
+	});
+
+}
+
+
+
+
+
+
+
+
+$("#searchbar").on('propertychange change keyup input paste', function(){
+	var searchString = $("#searchbar").val();
+	if (searchString == ""){
+		loadTweets();
+		return;
+	}
+	searchString = "/api/v1/tweet/search/" + searchString;
+	$.get( searchString, function( response ){
+		//console.log(response);
+		populateTweets(JSON.parse(response));
+	});
+
+});
+
+function populateTweets(tweets){
+	$('#tweets').empty();
+	for (tweet in tweets){
+		$('#tweets').append(toTweet(tweets[tweet].username,tweets[tweet].text,tweets[tweet].created));
+		( function (user) {
+			$('.'+user.username + 'tweet').click(function(){
+				loadTweetsOfUser(user.id, user.username, "");
+			});
+		})(tweets[tweet]);
+	}
+};
+
+function populateFollowers(users){
+	for (user in users){
+		$('#followers').append(toUser("followers" + user, users[user].follower, users[user].count));
+		( function (user) {
+			$('#followers'+ user + 'dialog').click(function(){
+				loadTweetsOfUser(users[user].follower_id, users[user].follower);
+			});
+		})(user);
+	}
+
+};
+
+function populateFollowing(users){
+	$('#following').empty();
+	for (user in users){
+		$('#following').append(toUser("following" + user, users[user].followee, users[user].count));
+		( function (user) {
+			$('#following'+ user + 'dialog').click(function(){
+				loadTweetsOfUser(users[user].followee_id, users[user].followee);
+			});
+		})(user);
+
+	}
+
+};
+
+
+function populateAll(users){
+	$('#all').empty();
+	for (user in users){
+		//	console.log(users[user].count);
+		$('#all').append(toUser("all" + user, users[user].username, users[user].count));
+		( function (user) {
+			$('#all'+ user + 'dialog').click(function(){
+				loadTweetsOfUser(users[user].id, users[user].username);
+			});
+		})(user);
+	}
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ assets/js/jquery.tweetbox.js
@@ -0,0 +1,131 @@
+(function($){
+
+	//browser detection from https://github.com/gabceb/jquery-browser-plugin
+	!function(a,b){"use strict";var c,d;if(a.uaMatch=function(a){a=a.toLowerCase();var b=/(opr)[\/]([\w.]+)/.exec(a)||/(chrome)[ \/]([\w.]+)/.exec(a)||/(version)[ \/]([\w.]+).*(safari)[ \/]([\w.]+)/.exec(a)||/(webkit)[ \/]([\w.]+)/.exec(a)||/(opera)(?:.*version|)[ \/]([\w.]+)/.exec(a)||/(msie) ([\w.]+)/.exec(a)||a.indexOf("trident")>=0&&/(rv)(?::| )([\w.]+)/.exec(a)||a.indexOf("compatible")<0&&/(mozilla)(?:.*? rv:([\w.]+)|)/.exec(a)||[],c=/(ipad)/.exec(a)||/(iphone)/.exec(a)||/(android)/.exec(a)||/(windows phone)/.exec(a)||/(win)/.exec(a)||/(mac)/.exec(a)||/(linux)/.exec(a)||/(cros)/i.exec(a)||[];return{browser:b[3]||b[1]||"",version:b[2]||"0",platform:c[0]||""}},c=a.uaMatch(b.navigator.userAgent),d={},c.browser&&(d[c.browser]=!0,d.version=c.version,d.versionNumber=parseInt(c.version)),c.platform&&(d[c.platform]=!0),(d.android||d.ipad||d.iphone||d["windows phone"])&&(d.mobile=!0),(d.cros||d.mac||d.linux||d.win)&&(d.desktop=!0),(d.chrome||d.opr||d.safari)&&(d.webkit=!0),d.rv){var e="msie";c.browser=e,d[e]=!0}if(d.opr){var f="opera";c.browser=f,d[f]=!0}if(d.safari&&d.android){var g="android";c.browser=g,d[g]=!0}d.name=c.browser,d.platform=c.platform,a.browser=d}(jQuery,window);
+
+	function stripTags(string,allowed){
+		return string.replace(/<\/?([a-z][a-z0-9]*)\b[^>]*>/gi,function($0,$1){return ''});
+	}
+
+	function ltrim(str) {
+		return str.replace(/^[ \\s\u00A0 ]+/g,'');
+	}
+
+	function rawText(str,trim){
+		var text=str
+	.replace(/<br><div>/gi,'\n') //chrome not taking in account white-space:pre
+	.replace(/<div><br><\/div>/gi,'\n') //chrome newline
+	.replace(/<br>&nbsp;/gi,'\n\n') //mozilla newline
+	.replace(/<div>|<br>|<\/p>/gi,'\n'); //html tags
+if($.browser.msie)
+	text=text.replace(/\n/g,' ').replace(/&nbsp;/g,' ').replace('  ',' ').replace('  ',' ');//ie shit
+text=trim?$.trim(stripTags(text)):ltrim(stripTags(text));
+return text;
+	}
+
+	var count=0;
+	$.fn.tweetbox=function(option){
+		if(typeof(option)=='object'||typeof(option)=='undefined'){
+			var args=(typeof(option)=='object'?[option]:[]);
+			return this.each(function(){
+				return methods.init.apply(this,args);
+			});
+		}else if(typeof option=='string'&&typeof methods[option]=='function'){
+			var args=Array.prototype.slice.call(arguments,1);
+			return this.each(function(){
+				return methods[option].apply(this,args);
+				console.error('Element is not a tweet box plugin');
+			});
+		}else console.error('Requested tweet box method not found.',option);
+	};
+
+	var methods={
+		init:function(settings){
+			var self=this;
+			this.settings=$.extend({
+				cb:false,
+				css:{'font-size':'inherit'},
+				default:false,
+					postfix:'',
+				limit:140,
+				highlight:[
+			{match:/\B@\w+/g,class:'mention'},
+				{match:/\B#\w+/g,class:'hashtag'},
+				{match:/(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/gi,class:'url'}
+			],
+				action:'tweet',
+				button:{
+					tweet:'Tweet',
+				reply:'Tweet Reply'
+				}
+			},settings);
+			this.index=count++;
+			var sourceText=this.settings.default||stripTags($.trim($(this).html().replace(/<br>/g,'\n')));
+			if($(this).css('position').toLowerCase()!='absolute')$(this).css('position','relative');
+			var cover={position:'absolute',top:'0px',right:'0px',bottom:'0px',left:'0px'};
+			this.previousText=rawText(sourceText);
+			this.$editor=$('<div contenteditable="true" id="editor" class="tweet-box-editable">').css(this.settings.css).css(cover).css({bottom:'40px',left:'0px','z-index':'99','overflow-x':'hidden','overflow-y':'auto','white-space':'pre',padding:'10px'});
+			$(this).css({overflow:'hidden',padding:'100px'}).html(this.$editor);
+
+			this.$editor.bind('keyup',function(e){
+				$(self).tweetbox('highlight',e.which);
+			});
+			this.$editor.bind('mouseup',function(e){
+				$(self).tweetbox('highlight');
+			});
+
+			setTimeout(function(){
+				self.$editor.focus();
+				$(self).tweetbox('highlight');
+			},50);
+
+			this.$button=$('<input type="button" id="tweet-box-button" class="tweet-box-button" value="Tweet">').css({position:'absolute',top:'4px',bottom:'5px',right:'1%',width:'20%',overflow:'hidden', height:'75%', padding:'2px'}).html(this.settings.button[this.settings.action]);
+			this.$info=$('<div class="tweet-box-info">').css({position:'absolute',top:'5px',left:'1%',width:'72%',top:'50%','text-align':'right',display:'table-cell','vertical-align':'middle'}).html('X Characters Left');
+			this.$bottom=$('<div class="tweet-box-bottom">').css(cover).css({top:'auto',height:'40px','z-index':'100','overflow':'hidden'}).append(this.$button,this.$info);
+			$(this).append(this.$bottom)
+
+				var size=this.$info.css('font-size');
+			if(size.substr(-2)=='px')size=size.substr(0,size.length-2);
+			this.$info.css('margin-top','-'+Math.round(+size/2)+'px');
+			this.$editor.html(sourceText);
+		},
+		highlight:function(keyCode){
+			var self=this;
+			var editor=this.$editor.get()[0];
+			var rSel=rangy.getSelection().saveCharacterRanges(editor);
+			var start=rSel[0].characterRange.start;
+			var end=rSel[0].characterRange.end;
+			var text;
+
+			var formatted=false;
+			//console.log('keyCode:',keyCode,'tl:',text.length,'pl:',this.previousText.length,'s:',rSel[0].characterRange.start,'e:',rSel[0].characterRange.end,text);
+			if(keyCode&&keyCode==13&&start==0){
+				text=this.previousText;
+			}else if(keyCode&&keyCode==13&&start==this.previousText.length+1){
+				text=ltrim(this.previousText)+'\n\n';
+			}else{
+				text=rawText(this.$editor.html());
+			}
+			var avaiable=(this.settings.limit-this.settings.postfix.length-text.length);
+			if(keyCode&&keyCode==13&&$.browser.msie){
+				if(start){
+					rSel[0].characterRange.start--;
+					rSel[0].characterRange.end--;
+				}
+				$(this).tweetbox('info','<span style="color:white">Sorry, multiple lines not supported by IE.</span>');
+			}else
+				$(this).tweetbox('info',avaiable<0?'<span style="color:red">'+avaiable+'</span>':avaiable);
+			formatted=text;
+			for(var i=0;i<this.settings.highlight.length;i++)
+				formatted=formatted.replace(this.settings.highlight[i].match,function(m){return '<span class="'+self.settings.highlight[i].class+'">'+m+'</span>'});
+			this.$editor.html(formatted||text);
+			this.previousText=text;
+			rangy.getSelection().restoreCharacterRanges(editor,rSel);
+		},
+		info:function(text){
+			this.$info.html(text);
+			return this;
+		}
+	};
+
+}(jQuery));
