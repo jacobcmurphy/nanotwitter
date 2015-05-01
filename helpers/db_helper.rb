7485b5fba63a6dc871009e0ca6c8b04c38eac330
Jacob Murphy <murphorum@gmail.com>
2015-04-30 20:43:51 -0400

    modified db helper

--- helpers/db_helper.rb
+++ helpers/db_helper.rb
@@ -1,8 +1,5 @@
 require 'sequel'
 
 module Database
-  def DB
-    @store ||= Sequel.connect(adapter: 'postgres', host: 'localhost', database: 'postgres', user: 'edenzik')
-    @store
-  end
+  DB = Sequel.connect(adapter: 'postgres', host: 'localhost', database: 'postgres', user: 'edenzik')
 end
