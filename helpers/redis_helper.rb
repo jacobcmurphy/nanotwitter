b9c6afb1c45c74e305cf9e8c2c769bc5a445aec0
Eden Zik <edenzik@gmail.com>
2015-05-01 02:22:29 -0400

    added something automatically

--- helpers/redis_helper.rb
+++ helpers/redis_helper.rb
@@ -2,14 +2,14 @@ require 'redis'
 
 module RedisConnect
 
-  def get_from_redis(key)
-	r ||= Redis.new
-     if r.get(key).nil?
-      result =  yield
-      r.set key, result
-      #r.expire key, 10
-      return r.get key
-    end
-    return r.get key
-  end
+	def get_from_redis(key)
+		r ||= Redis.new
+		if r.get(key).nil?
+			result =  yield
+			r.set key, result
+			r.expire key, 20
+			return r.get key
+		end
+		r.get key
+	end
 end
